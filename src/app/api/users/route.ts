a3a2a9b43d590c352df3009167b122447e00d74a
unknown <you@example.com>
2025-07-16 16:31:38 +0800

    Implemented API Creation and API Usage

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+
+export async function GET() {
+    try {
+        const allUser = await db.select().from(users);
+        return NextResponse.json(allUser, {status: 200 });
+    } 
+    catch (error) {
+        console.error("Error fetching users:", error);
+        return NextResponse.json({ error: "Failed to fetch users" }, { status:  500 });
+    }
+}
